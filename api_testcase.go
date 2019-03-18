48db3d58beb61be5889ed90a7a6b4b2ac39e979f
Simon Frey <simon@programmfabrik.de>
2019-03-18 11:26:07 +0100

    Revert "Reponse in hard struct with statuscode and body, to have a json error"
    This reverts commit 8a26a988c660dd4ec3b306d11ad7fc2edf134028.

--- api_testcase.go
+++ api_testcase.go
@@ -23,10 +23,9 @@ const (
 // Case defines the structure of our single testcase
 // It gets read in by our config reader at the moment the mainfest.json gets parsed
 type Case struct {
-	Name              string            `json:"name"`
-	RequestData       *util.GenericJson `json:"request"`
-	TypedResponseData *CaseResponse     `json:"response"`
-	responseData      util.GenericJson
+	Name              string                 `json:"name"`
+	RequestData       *util.GenericJson      `json:"request"`
+	ResponseData      util.GenericJson       `json:"response"`
 	ContinueOnFailure bool                   `json:"continue_on_failure"`
 	Store             map[string]interface{} `json:"store"`                // init datastore before testrun
 	StoreResponse     map[string]string      `json:"store_response_qjson"` // store qjson parsed response in datastore
@@ -50,11 +49,6 @@ type Case struct {
 	ServerURL string
 }
 
-type CaseResponse struct {
-	Statuscode int              `json:"statuscode"`
-	Body       util.GenericJson `json:"body"`
-}
-
 func (testCase Case) runAPITestCase() (success bool) {
 	if testCase.LogVerbosity != nil && *testCase.LogVerbosity > FylrConfig.Apitest.LogVerbosity {
 		defer FylrConfig.SetLogVerbosity(FylrConfig.Apitest.LogVerbosity)
@@ -69,22 +63,6 @@ func (testCase Case) runAPITestCase() (success bool) {
 		log.Infof("     [%2d] '<no name>'", testCase.index)
 	}
 
-	//Marshal external datastructure in our internal used reponseData
-	jsonResponse, err := json.Marshal(testCase.TypedResponseData)
-	if err != nil {
-		err = fmt.Errorf("error marshaling TypedResponseData: %s", err)
-		r.SaveToReportLog(fmt.Sprintf("Error during execution: %s", err))
-		log.Errorf("     [%2d] %s", testCase.index, err)
-		return false
-	}
-	err = json.Unmarshal(jsonResponse, &testCase.responseData)
-	if err != nil {
-		err = fmt.Errorf("error unmarshaling into responseData: %s", err)
-		r.SaveToReportLog(fmt.Sprintf("Error during execution: %s", err))
-		log.Errorf("     [%2d] %s", testCase.index, err)
-		return false
-	}
-
 	r.NewChild(testCase.Name)
 
 	start := time.Now()
@@ -96,7 +74,7 @@ func (testCase Case) runAPITestCase() (success bool) {
 		log.Errorf("     [%2d] %s", testCase.index, err)
 		return false
 	}
-	err = testCase.dataStore.SetMap(testCase.Store)
+	err := testCase.dataStore.SetMap(testCase.Store)
 	if err != nil {
 		err = fmt.Errorf("error setting datastore map:%s", err)
 		r.SaveToReportLog(fmt.Sprintf("Error during execution: %s", err))
@@ -395,10 +373,10 @@ func (testCase Case) loadRequest() (req api.Request, err error) {
 
 func (testCase Case) loadResponse() (res api.Response, err error) {
 	// unspecified response is interpreted as status_code 200
-	if testCase.responseData == nil {
+	if testCase.ResponseData == nil {
 		return api.NewResponse(200, nil, bytes.NewReader([]byte("")))
 	}
-	spec, err := testCase.loadResponseSerialization(testCase.responseData)
+	spec, err := testCase.loadResponseSerialization(testCase.ResponseData)
 	if err != nil {
 		return res, fmt.Errorf("error loading response spec: %s", err)
 	}
