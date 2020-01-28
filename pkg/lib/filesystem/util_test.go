d5dcc220a05098294672a8c544f6d5807d182b46
Martin Rode <martin.rode@programmfabrik.de>
2020-01-28 16:11:37 +0100

    Removed an unused function "CopyFileBetweenFileSystems".

--- pkg/lib/filesystem/util_test.go
+++ pkg/lib/filesystem/util_test.go
@@ -1,46 +1,12 @@
 package filesystem
 
 import (
-	"fmt"
-	"github.com/spf13/afero"
-	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strconv"
 	"testing"
+
+	"github.com/spf13/afero"
 )
-
-func TestCopyFileBetweenFileSystems(t *testing.T) {
-	outFS := afero.NewMemMapFs()
-	inFS := afero.NewMemMapFs()
-
-	inPath := filepath.Join(os.TempDir(), "tmpfile.tmp")
-	outPath := filepath.Join(afero.GetTempDir(inFS, ""), "tmpfile.tmp")
-
-	err := CopyFileBetweenFileSystems(inFS, outFS, inPath, outPath, 644)
-	if err.Error() != fmt.Sprintf("open %s: file does not exist", inPath) {
-		t.Errorf("Unexpected Error: %s. Expected: %s", err, fmt.Sprintf("open %s: file does not exist", inPath))
-	}
-
-	inFS.Create(inPath)
-
-	err = CopyFileBetweenFileSystems(inFS, outFS, inPath, outPath, 644)
-	if err != nil {
-		t.Errorf("Could not copy file: %s", err)
-	}
-
-	//Check if the files are the same
-	fileIn, _ := inFS.OpenFile(inPath, os.O_RDONLY, 644)
-	fileOut, _ := outFS.OpenFile(outPath, os.O_RDONLY, 644)
-
-	fileInContent, _ := ioutil.ReadAll(fileIn)
-	fileOutContent, _ := ioutil.ReadAll(fileOut)
-
-	if string(fileInContent) != string(fileOutContent) {
-		t.Error("InFile != OutFile")
-	}
-
-}
 
 func TestMemFS(t *testing.T) {
 	Fs = afero.NewMemMapFs()
