2a86fd03129413a6f9102c28a03651e341881c35
Philipp Hempel <philipp.hempel@programmfabrik.de>
2020-10-27 11:43:17 +0100

    simplified apitest for file_sqlite template: bounce marshalled result of SELECT and compare to json file; see #34

--- test/file_sqlite/setup_sqlite.sql
+++ test/file_sqlite/setup_sqlite.sql
@@ -1,28 +1,19 @@
-DROP TABLE IF EXISTS "test_endpoints";
+DROP TABLE IF EXISTS "test_values";
 
-CREATE TABLE "test_endpoints" (
-    "endpoint" TEXT NOT NULL,
-    "method" TEXT NOT NULL DEFAULT 'GET',
-    "file" TEXT,
-    "format" BLOB,
-    "param1"  TEXT,
-    "param2"  INTEGER,
-    "param3"  NUMERIC,
-    "expected_statuscode" INTEGER NOT NULL DEFAULT 200,
-    "expected_body" BLOB
+CREATE TABLE "test_values" (
+    "text" TEXT NOT NULL,
+    "number"  INTEGER,
+    "bool"  NUMERIC
 );
 
-INSERT INTO "test_endpoints" ("endpoint", "method", "param1", "param2", "param3", "expected_body")
-VALUES ('bounce-json', 'POST', 'abc', 123, true, '{"query_params": {"param1": ["abc"],"param2": ["123"],"param3": ["1"]}}');
+INSERT INTO "test_values" ("text", "number", "bool")
+VALUES ('one', 1, true);
 
-INSERT INTO "test_endpoints" ("endpoint", "method", "param1", "expected_body")
-VALUES ('bounce-json', 'POST', 'xyz', '{"query_params": {"param1": ["xyz"]}}');
+INSERT INTO "test_values" ("text", "number", "bool")
+VALUES ('two', 2, false);
 
-INSERT INTO "test_endpoints" ("endpoint", "method", "param1", "param2", "param3", "file", "format", "expected_body")
-VALUES ('bounce', 'POST', 'abc', 123, true, '../_res/assets/camera.jpg', '{"pre_process": {"cmd": {"name": "exiftool","args": ["-j", "-g", "-"]}}}', '[{"SourceFile": "-","ExifTool": {},"JFIF": {},"ICC_Profile": {},"File": {},"Composite": {}}]');
+INSERT INTO "test_values" ("text", "number")
+VALUES ('three', 3);
 
-INSERT INTO "test_endpoints" ("endpoint", "expected_statuscode")
-VALUES ('wrong/path.jpg', 404);
-
-INSERT INTO "test_endpoints" ("endpoint", "format")
-VALUES ('assets/camera.jpg', '{"pre_process": {"cmd": {"name": "exiftool","args": ["-j", "-g", "-"]}}}');
+INSERT INTO "test_values" ("text")
+VALUES ('four');
