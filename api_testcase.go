fa312590a82173b62f845023d7d4c9c698fb08f0
Unai Garcia <unai.garcia@programmfabrik.de>
2021-09-14 16:56:16 +0200

    fixed tests and response logic checks

--- api_testcase.go
+++ api_testcase.go
@@ -263,8 +263,7 @@ func (testCase Case) executeRequest(counter int) (compare.CompareResult, api.Req
 	// If we don't define an expected response, we won't have a format
 	// That's problematic if the response is not JSON, as we try to parse it for the datastore anyway
 	// So we don't fail the test in that edge case
-	if err != nil && (testCase.ResponseData != nil || testCase.CollectResponse != nil ||
-		len(testCase.BreakResponse) > 0 || len(testCase.StoreResponse) > 0) {
+	if err != nil && (testCase.ResponseData != nil || len(testCase.StoreResponse) > 0) {
 		testCase.LogReq(req)
 		err = fmt.Errorf("error getting json from response: %s", err)
 		return responsesMatch, req, apiResp, err
@@ -486,7 +485,7 @@ func (testCase Case) responsesEqual(expected, got api.Response) (compare.Compare
 	if err != nil {
 		return compare.CompareResult{}, fmt.Errorf("error loading expected generic json: %s", err)
 	}
-	if testCase.ResponseData == nil && testCase.CollectResponse == nil && len(testCase.BreakResponse) == 0 {
+	if len(expected.Body()) < 1 {
 		expected.Format.IgnoreBody = true
 	}
 	gotJSON, err := got.ServerResponseToGenericJSON(expected.Format, false)
